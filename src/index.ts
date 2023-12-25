965306b9205fafaf35393e8915db7829471b254b
Nicolai Christensen <nicolaibernhardchristensen@gmail.com>
2023-12-25 23:49:13 +0100

    exit process after and use the utility added beforehand

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 import { get_users_package_manager } from "@/utils/get_package_manager.js";
 import { logger } from "@/utils/logging.js";
 import { run_the_cli } from "./cli/index.js";
+import { parse_name_and_path } from "./utils/parse_name_and_path.js";
 
 async function main() {
   // For the future, get the version of the CLI and check if it's up to date, if not then prompt the user to update
@@ -15,11 +16,15 @@ async function main() {
     flags: { import_alias, no_git, no_install },
   } = await run_the_cli();
 
+  const [scopedAppName, appDir] = parse_name_and_path(app_name);
+
   logger.info(`Creating new Maldini project in ${app_name}`);
   logger.info(`Using ${import_alias} as import alias`);
   logger.info(`Git: ${no_git ? "No" : "Yes"}`);
   logger.info(`Install: ${no_install ? "No" : "Yes"}`);
   logger.info(`HTTP Framework: ${http_framework}`);
+
+  process.exit(0);
 }
 
 main().catch((process_error) => {
