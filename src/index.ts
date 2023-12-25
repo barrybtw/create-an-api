2a4a97d27a693fe9fb36feef5bb5e97da53cac6d
Nicolai Christensen <nicolaibernhardchristensen@gmail.com>
2023-12-25 22:48:37 +0100

    chore: use alias

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-import { get_users_package_manager } from "./utils/get_package_manager.js";
-import { logger } from "./utils/logging.js";
+import { get_users_package_manager } from "@/utils/get_package_manager.js";
+import { logger } from "@/utils/logging.js";
 
 async function main() {
-  //
+  // For the future, get the version of the CLI and check if it's up to date, if not then prompt the user to update
   const package_manager = get_users_package_manager();
   logger.info(`Using ${package_manager} as package manager`);
 }
